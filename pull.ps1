0bddc3bbd358522a85c06d2b6cff9eab187b2233
Bruce Shiundu <bruce.shiundu@sap.com>
2022-01-21 00:13:10 +0100

    Implemented simple script to pull image

---
+++ pull.ps1
@@ -0,0 +1,20 @@
+﻿#i539683/simplewhale:latest
+#Simple powershell script to pull an image from the dockerhub
+# Image name lastcommitimage
+# Docker id : 
+# Password : 
+
+Write-Host "To pull an image please provide the docker login credentials"
+
+$docker_id= Read-Host -Prompt "Docker id: "
+$password= Read-Host -Prompt "Password : "
+
+docker login -u $docker_id --password $password
+
+Write-Host "Please provide the image name and version"
+$image_name= Read-Host -Prompt "Image name: "
+
+docker pull $image_name
+# docker stop simple_app
+
+docker run --name simple_app -dp 5000:5000 $image_name
